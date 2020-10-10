79ee3f026c3f4a908e4d433c7f8624b0e7d519d7
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-10 18:26:12 -0500

    Commit inicial.

---
+++ src/HiLo.java
@@ -0,0 +1,13 @@
+public class HiLo {
+    public static void main(String[] args) {
+        /*
+        *Comentarios traducidos al español desde lo expuesto en el libro
+        *Variables cambiadas a mi gusto y al español.
+        * */
+
+        // Crea un numero random para que el usuario adivine.
+        int numero = (int)(Math.random() * 100 + 1);
+        System.out.println("numero = " + numero);
+
+    }
+}
