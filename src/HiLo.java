13a1048221d44fa881b1fee1f6bc6102a13c86c7
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-11 11:55:06 -0500

    Numero de intentos

--- src/HiLo.java
+++ src/HiLo.java
@@ -13,15 +13,22 @@ public class HiLo {
 			int numero = (int) (Math.random() * 500 - 250);
 			//System.out.println("numero = " + numero);
 			int prediccion = 0;
+			int numeroDeIntentos = 0;
 			while (prediccion != numero) {
 				System.out.println("Adivina un numero entre -250 y 250: ");
 				prediccion = scanner.nextInt();
-				if (prediccion < numero)
+				if (prediccion < numero) {
 					System.out.println(prediccion + " es muy bajo. Inténtelo de nuevo.");
-				else if (prediccion > numero)
+					numeroDeIntentos+=1;
+				}
+				else if (prediccion > numero) {
 					System.out.println(prediccion + " es muy alto. Inténtelo de nuevo.");
-				else
+					numeroDeIntentos+=1;
+				}
+				else {
 					System.out.println(prediccion + " es correcto. ¡Ganaste!");
+					System.out.println("El numero de intentos fue de: "+numeroDeIntentos);
+				}
 			} //Fin del ciclo mientras/while para adivinar
 			System.out.println("¿Te gustaría jugar nuevamente (s/n)?");
 			jugarNuevamente = scanner.next();
