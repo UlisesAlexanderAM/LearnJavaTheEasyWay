4be3b1b2753a4a2e70f75efff940b2f8d4ca4d10
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-11 11:08:52 -0500

    Ciclos: Pregunta, Verifica y Repite.

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
+
 public class HiLo {
-    public static void main(String[] args) {
-        /*
-        *Comentarios traducidos al español desde lo expuesto en el libro
-        *Variables cambiadas a mi gusto y al español.
-        * */
+	public static void main(String[] args) {
+		/*
+		 *Comentarios traducidos al español desde lo expuesto en el libro
+		 *Variables cambiadas a mi gusto y al español, cuando crei necesario.
+		 * */
+		Scanner scanner = new Scanner(System.in);
+		// Crea un numero random para que el usuario adivine.
+		int numero = (int)(Math.random() * 100 + 1);
+//        System.out.println("numero = " + numero);
+		int prediccion=0;
+		while (prediccion != numero) {
+			System.out.println("Adivina un numero entre 1 y 100: ");
+			prediccion = scanner.nextInt();
+			System.out.println("Ingresaste " + prediccion + ".");
+		}
 
-        // Crea un numero random para que el usuario adivine.
-        int numero = (int)(Math.random() * 100 + 1);
-        System.out.println("numero = " + numero);
-
-    }
+	}
 }
