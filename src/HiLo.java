8d63d618ef9f31149112a54c9dc900af9a04b66d
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-11 11:41:14 -0500

    Ciclo jugar nuevamente.

--- src/HiLo.java
+++ src/HiLo.java
@@ -7,19 +7,26 @@ public class HiLo {
 		 *Variables cambiadas a mi gusto y al español, cuando crei necesario.
 		 * */
 		Scanner scanner = new Scanner(System.in);
-		// Crea un numero random para que el usuario adivine.
-		int numero = (int)(Math.random() * 100 + 1);
-		//System.out.println("numero = " + numero);
-		int prediccion=0;
-		while (prediccion != numero) {
-			System.out.println("Adivina un numero entre 1 y 100: ");
-			prediccion = scanner.nextInt();
-			if (prediccion < numero)
-				System.out.println(prediccion + " es muy bajo. Inténtelo de nuevo.");
-			else if (prediccion > numero)
-				System.out.println(prediccion + " es muy alto. Inténtelo de nuevo.");
-			else
-				System.out.println(prediccion + " es correcto. ¡Ganaste!");
-		}
+		String jugarNuevamente;
+		do {
+			// Crea un numero random para que el usuario adivine.
+			int numero = (int) (Math.random() * 100 + 1);
+			//System.out.println("numero = " + numero);
+			int prediccion = 0;
+			while (prediccion != numero) {
+				System.out.println("Adivina un numero entre 1 y 100: ");
+				prediccion = scanner.nextInt();
+				if (prediccion < numero)
+					System.out.println(prediccion + " es muy bajo. Inténtelo de nuevo.");
+				else if (prediccion > numero)
+					System.out.println(prediccion + " es muy alto. Inténtelo de nuevo.");
+				else
+					System.out.println(prediccion + " es correcto. ¡Ganaste!");
+			} //Fin del ciclo mientras/while para adivinar
+			System.out.println("¿Te gustaría jugar nuevamente (s/n)?");
+			jugarNuevamente = scanner.next();
+		} while (jugarNuevamente.equalsIgnoreCase("s"));
+		System.out.println("¡Gracias por jugar! Adios.");
+		scanner.close();
 	}
 }
