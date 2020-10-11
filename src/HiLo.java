d1b12874ea22aed80dad3e0ab8792d1ccb3f148d
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-11 11:24:26 -0500

    Sentencias if : Probando para la condiciones correctas.

--- src/HiLo.java
+++ src/HiLo.java
@@ -9,13 +9,17 @@ public class HiLo {
 		Scanner scanner = new Scanner(System.in);
 		// Crea un numero random para que el usuario adivine.
 		int numero = (int)(Math.random() * 100 + 1);
-//        System.out.println("numero = " + numero);
+		//System.out.println("numero = " + numero);
 		int prediccion=0;
 		while (prediccion != numero) {
 			System.out.println("Adivina un numero entre 1 y 100: ");
 			prediccion = scanner.nextInt();
-			System.out.println("Ingresaste " + prediccion + ".");
+			if (prediccion < numero)
+				System.out.println(prediccion + " es muy bajo. Inténtelo de nuevo.");
+			else if (prediccion > numero)
+				System.out.println(prediccion + " es muy alto. Inténtelo de nuevo.");
+			else
+				System.out.println(prediccion + " es correcto. ¡Ganaste!");
 		}
-
 	}
 }
